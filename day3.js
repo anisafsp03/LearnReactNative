88cacd01dfccfa44995ff12911b8650247971bc5
anisafsp03 <anisafebryantisucipramesti@gmail.com>
2022-02-18 20:54:28 +0700

    belajarJS

---
+++ day3.js
@@ -0,0 +1,15 @@
+
+/*
+function cekNilai(nilai){
+  if(nilai>=1&&nilai<=100){
+    if(nilai>=70){
+      return "lulus"
+    }else if(nilai<70){
+      return "tidak lulus"
+    }else{
+      return "nilai tidak diantara 1-100"
+    }
+  }
+}
+console.log(cekNilai(105))
+*/
---
+++ index.js
@@ -0,0 +1,31 @@
+let nama = 'Anisa';
+const nilai1 = 87;
+const nilai2 = 89;
+const nilai3 = 95;
+
+
+if(nilai1 >= 90  && nilai2 >= 90 && nilai3 >= 90){
+    console.log(nama + ' ' + 'nilai anda sudah baik untuk mendaftar snmptn')
+    }else if(nilai1 >= 80 || nilai2 >= 90 || nilai3 >= 90){
+        console.log(nama + ' ' + 'nilai anda cukup baik untuk mendaftar snmptn')
+        }else{
+            console.log(nama + ' ' + 'silahkan tingkatkan lagi nilai anda untuk berpeluang lolos snmptn')
+        } 
+
+
+/*
+if(!nilai1 <= 85){
+    console.log('nilai sudah baik')}
+*/
+
+
+ /*
+console.log(nama + ' ' + umur );
+
+if(lulussekolah){
+    console.log('lulus sma')
+}else{
+    console.log('belum lulus')
+}
+
+console.log(sekolah) */
